b9b256a2836268f11dd46a8dd70ccaf8d4ac6377
pkaterina <pes1@newbie.ispras.ru>
2020-03-06 14:01:12 +0300

    Исправлен алгоритм фильтрации HTML-строк

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /*Условие ИЗ№1:
  * Составить программу построчной фильтрации текста, вводимого пользователем.
  * Суть фильтра — отбор строк, содержащих HTML-тег с заданным значением атрибута src.
@@ -14,49 +14,51 @@ int FilterHTMLString(char **string, int N);
 
 int main() {
   int N = 2;
-  int M = 5;
-
   char *string = "<img src=\"file.format\" alt=\"text\" />";
-  char *string1 = "<img src=\"\" alt=\"text\" />";
 
-  char **v = (char **)malloc(N * sizeof(char *));
-  for (int i = 0; i < N; i++) {
-	v[i] = (char *)malloc(M * sizeof(char));
+  unsigned long M = strlen(string);
+  char **v = (char **)malloc(N * sizeof(char *)); //количество строк
+  for (int i = 0; i < N; i++) { //столбцов
+	v[i] = (char *)malloc((M + 1) * sizeof(char));
   }
 
   for (int i = 0; i < N; i++) {
-	for (int j = 0; j < M; j++) {
-	  v[j] = string;
-	  printf("%s %d %d\n", v[j], i, j);
-	}
+	 for (int j = 0; j < M; j++) {
+	   v[i][j] = string[j];
+	 }
   }
 
-  printf("quantity of strings with src attrs = %d",  FilterHTMLString(*v, N));
+  printf("quantity of strings with src attrs = %d",  FilterHTMLString(v, N));
+
+  for (int i = 0; i < N; i++) {
+	free(v[i]);
+  }
+  free(v);
   return 0;
 }
-
 
 int FilterHTMLString(char **string, int N) {
 
   int src_len = 3;
   int quantity_src_attributes = 0;
 
-  printf("string = %s \n", string);
-  for (int j = 0; j < N; j++){
-	int i = 0;
-	while (string[i] != '\0') {
-	  printf("%c i = %d\n", string[i], i);
-	  if ((string[i] == 's') && (string[i + 1] == 'r') && (string[i + 2] == 'c')) {
+  for (int i = 0; i < N; i++){
+    int j = 0;
+    while (string[i][j] != '\0'){
+	  if ((int)(string[i][j] == 's') && ((int)string[i][j + 1] == 'r') && ((int)string[i][j + 2] == 'c')) {
+		printf("string[%d %d] = %c\n", i, j, string[i][j]);
 		printf("src\n");
-		i += src_len;
+		j += src_len;
 
-		if ((string[i] == '=') && (string[i + 1] == '"') && (string[i + 2] != '"')) {
+		if (((int)string[i][j] == '=') && ((int)string[i][j + 1] == '"') && ((int)string[i][j + 2] != '"')) {
 		  quantity_src_attributes++;
 		  break;
 		}
 	  }
-	  i++;
-	}
+	  j++;
+    }
   }
   return quantity_src_attributes;
 }
+
+
